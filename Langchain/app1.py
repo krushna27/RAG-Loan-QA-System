1350f59bbab36f2c40b0b08ae50e865e2b1f653b
Krushnakant Bankar <76439755+krushna27@users.noreply.github.com>
2025-07-21 00:34:07 +0530

    Add files via upload

---
+++ Langchain/app1.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import time
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.schema import Document
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+
+
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+st.title("RAG Application built on Gemini Model")
+
+with open("cleaned_data2.txt","r", encoding="utf-8")as file:
+    data = file.read()
+
+data1 = [Document(page_content=data)]
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+docs = text_splitter.split_documents(data1)
+
+
+
+embedding_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+vectorstore = FAISS.from_documents(docs, embedding_model)
+
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 10})
+
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash",temperature=0,max_tokens=None,timeout=None)
+
+
+query = st.chat_input("Say something: ") 
+prompt = query
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+if query:
+    question_answer_chain = create_stuff_documents_chain(llm, prompt)
+    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+    response = rag_chain.invoke({"input": query})
+    #print(response["answer"])
+
+    st.write(response["answer"])
+
+
